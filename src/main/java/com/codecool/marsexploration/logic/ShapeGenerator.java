b099db7fb7f10f149ed4773a0452c4f42e716ecd
dudasdominik <dudasdominik3@gmail.com>
2023-04-17 12:02:38 +0200

    add generateShape method in ShapeGenerator

--- src/main/java/com/codecool/marsexploration/logic/ShapeGenerator.java
+++ src/main/java/com/codecool/marsexploration/logic/ShapeGenerator.java
@@ -1,17 +1,32 @@
 package com.codecool.marsexploration.logic;
 
+import com.codecool.marsexploration.data.Coordinate;
 import com.codecool.marsexploration.data.Symbol;
 
 import java.util.Random;
+import java.util.List;
 
 public class ShapeGenerator {
     private int amount;
-    private Symbol symbol;
+    private char[][] area;
     private Random random;
 
-    public ShapeGenerator(int amount, Symbol symbol) {
+    public ShapeGenerator(int amount) {
         this.amount = amount;
-        this.symbol = symbol;
         this.random = new Random();
+        this.area = new char[amount][amount];
+    }
+
+    public List<Coordinate> generateShape() {
+        int randomX = random.nextInt(amount);
+        int randomY = random.nextInt(amount);
+
+        area[randomX][randomY] = 'x';
+
+        int placedCounter = 1;
+
+        while (placedCounter < amount){
+
+        }
     }
 }
