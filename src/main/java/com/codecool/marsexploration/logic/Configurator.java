e55ab03213642e3ff968c956cf5456408ed6da5c
dudasdominik <dudasdominik3@gmail.com>
2023-04-17 11:26:19 +0200

    create Configurator class

---
+++ src/main/java/com/codecool/marsexploration/logic/Configurator.java
@@ -0,0 +1,11 @@
+package com.codecool.marsexploration.logic;
+
+import java.util.List;
+import java.util.Map;
+
+public class Configurator {
+    private int width;
+    private String filename;
+    private List<Character> symbol;
+    private Map<Character, Integer> items;
+}
