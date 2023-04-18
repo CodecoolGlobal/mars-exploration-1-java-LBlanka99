dc3a58b0dc228e4af2a8df955e66b74c6cc5b0b5
dudasdominik <dudasdominik3@gmail.com>
2023-04-18 15:12:22 +0200

    make logic for Configurator class which works

--- src/main/java/com/codecool/marsexploration/Application.java
+++ src/main/java/com/codecool/marsexploration/Application.java
@@ -1,11 +1,22 @@
 package com.codecool.marsexploration;
 
+import com.codecool.marsexploration.logic.Configurator;
+
+import java.util.List;
 import java.util.Random;
+import com.codecool.marsexploration.data.Map;
 
 public class Application {
     public static void main(String[] args) {
-        Random random = new Random();
-        System.out.println(random.nextInt(42));
 
+        Configurator configurator = new Configurator(35, "nev");
+        Map map = configurator.drawMap(List.of(10, 20, 30), List.of(5, 15), 8, 12);
+
+        for (int i = 0; i < map.getWidth(); i++) {
+            for (int j = 0; j < map.getWidth(); j++) {
+                System.out.print(map.getMap()[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
